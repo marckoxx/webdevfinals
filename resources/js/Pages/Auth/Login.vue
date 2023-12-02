4046d20f3c1eabdc30023d58b8086a6dd18c3bb1
Stib <152588332+StibGee@users.noreply.github.com>
2023-12-02 17:49:43 +0800

    test

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -33,13 +33,34 @@ const submit = () => {
     <GuestLayout>
         <div class="container">
 
+<<<<<<< Updated upstream
             <Head title="Log in" />
 
             <div v-if="status" class="mb-4 font-medium text-sm text-success">
                 <!-- Bootstrap does not have a direct equivalent to Tailwind's text-green-600. You may adjust the color using Bootstrap classes or inline styles. -->
                 {{ status }}
+=======
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+        <div class=" row mx-5 g-lg-5 py-5">
+            <div class="img-cont col-lg-7 text-center text-lg-start">
+                <img src="images/logo/logo3.png" alt="" class="img-fluid">
+>>>>>>> Stashed changes
             </div>
+            <div class="form col-md-6 col-lg-5">
+                <div class="d-flex justify-content-center">
+                    <ul class="nav mb-3" id="loginSignupTabs" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="login-tab" data-bs-toggle="pill" href="#login" role="tab" aria-controls="login" aria-selected="true">Login</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="signup-tab" data-bs-toggle="pill" href="#signup" role="tab" aria-controls="signup" aria-selected="false">Sign Up</a>
+                    </li>
+                    </ul>
+                </div>
 
+<<<<<<< Updated upstream
             <form @submit.prevent="submit">
                 <div class="mb-3">
                     <InputLabel for="email" value="Email" />
@@ -77,5 +98,178 @@ const submit = () => {
                 </div>
             </form>
         </div>
+=======
+                <div class="tab-content" id="loginSignupTabContent">
+                    <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+                        <form  class="p-4 p-md-5 border rounded-3" @submit.prevent="submit" >
+                            <!--email-->
+                            <div class="form-floating mb-3">
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    class="form-control"
+                                    v-model="form.email"
+                                    required
+                                    autofocus
+                                    autocomplete="username"
+                                />
+                                <InputLabel for="email" value="Email" />
+                                <div class="mt-2">
+                                <InputError class="mt-2" :message="form.errors.email" />
+                                </div>
+                            </div>
+                            <!--password-->
+                            <div class="form-floating mb-3">
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    class="form-control"
+                                    v-model="form.password"
+                                    required
+                                    autocomplete="current-password"
+                                />
+                                <InputLabel for="password" value="Password" />
+                                <div class="mt-2">
+                                    <InputError class="mt-2" :message="form.errors.password" />
+                                </div>
+                            </div>
+                        <div class="block mt-4">
+                            <label class="flex items-center">
+                                <Checkbox name="remember" v-model:checked="form.remember" />
+                                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                            </label>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <Button class="btn w-100" style="font-weight: bold;" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                Log in
+                            </Button>
+                            <Link
+                                v-if="canResetPassword"
+                                :href="route('password.request')"
+                                class="underline text-sm text-black dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            >
+                                Forgot your password?
+                            </Link>
+                            </div>
+                         </form>
+                    </div>
+                    <div class="tab-pane fade show active" id="signup" role="tabpanel" aria-labelledby="signup-tab">
+                        <form  class="p-4 p-md-5  " @submit.prevent="submit" >
+                            <div class="form-floating mb-3">
+                                <TextInput
+                                    id="name"
+                                    type="text"
+                                    class="form-control"
+                                    v-model="form.name"
+                                    required
+                                    autofocus
+                                    autocomplete="name"
+                                />
+                                <InputLabel for="name" value="Name" />
+                                <div class="mt-2">
+                                    <InputError class="mt-2" :message="form.errors.name" />
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    class="form-control"
+                                    v-model="form.email"
+                                    required
+                                    autocomplete="username"
+                                />
+                                <InputLabel for="email" value="Email" />
+                                <div class="mt-2">
+                                    <InputError class="mt-2" :message="form.errors.email" />
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    class="form-control"
+                                    v-model="form.password"
+                                    required
+                                    autocomplete="new-password"
+                                />
+                                <InputLabel for="password" value="Password" />
+                                <div class="mt-2">
+                                    <InputError class="mt-2" :message="form.errors.password" />
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <TextInput
+                                    id="password_confirmation"
+                                    type="password"
+                                    class="form-control"
+                                    v-model="form.password_confirmation"
+                                    required
+                                    autocomplete="new-password"
+                                />
+                                <InputLabel for="password_confirmation" value="Confirm Password" />
+
+                                <div class="mt-2">
+                                    <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                                </div>
+                            </div>
+
+                            <div class="block mt-4">
+                                <Button class="btn w-100" style="font-weight: bold;" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                    Register
+                                </Button>
+                                <Link
+                                    :href="route('login')"
+                                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                >
+                                    Already registered?
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+>>>>>>> Stashed changes
     </GuestLayout>
 </template>
+<style scoped>
+    .btn{
+        border-radius: 10px 0 10px 0;
+        border: 0;
+        color: #A555EC;
+    --c: no-repeat linear-gradient(#A555EC 0 0);
+    background:
+        var(--c)  calc(-101% + var(--p,0%)) 100%,
+        var(--c)  calc( 201% - var(--p,0%)) 0;
+    background-size: 50.1% var(--p,.08em);
+    transition: .3s var(--t,0s), background-position .3s calc(.3s - var(--t,0s))
+    }
+    .btn:hover {
+    --p: 101%;
+    --t: 0.3s;
+    color: #FFFFD0;
+    font-weight: 300;
+    }
+    .form {
+        height: 40em;
+        border: 1px solid #A555EC;
+        background-color: rgba(208, 156, 250, 0.5); /* Using rgba to set opacity only for the background */
+        border-radius: 15px;
+        padding: 40px;
+    }
+    #loginSignupTabs .nav-link {
+        color: black; /* Change to the desired text color */
+        border-color: gray; /* Change to the desired border color */
+        transition: background-color 0.3s ease; /* Add a smooth transition effect */
+    }
+
+
+    #loginSignupTabs .nav-link.active {
+        border-bottom: 3px solid;
+        color: #A555EC; /* Change to the desired text color for active tab */
+    }
+</style>
+
--- resources/js/Pages/showcase.vue
+++ resources/js/Pages/showcase.vue
@@ -29,12 +29,12 @@ const getImagePath = (motor) => {
             <Link class="btn btn-dark" :href="route('catalog.index')">
                 Go Back
             </Link>
-            <div class="row">
+            <div class="row mt-4">
+                <h4 class="my-3 fw-bold">Reservation</h4>
                 <div class="col-lg-6 py-lg-4 ps-lg-5">
-                    <img :src="getImagePath(motorcycle)" :alt="motorcycle.model" class="card-img-top w-100"
+                    <img :src="getImagePath(motorcycle)" :alt="motorcycle.model" class="img img-fluid fixed-height-image"
                     style="object-fit: cover; max-height: 500px;">
-                </div>
-                        <div class="col-lg-6 py-lg-4 ps-lg-5">
+                    <div class="col-lg-6 py-lg-4 ps-lg-5">
                             <p class="card-text">Brand: {{ motorcycle.brand.name }}</p>
                             <p class="card-text">Model: {{ motorcycle.model }}</p>
                             <p class="card-text">Type: {{ motorcycle.type.name }}</p>
@@ -47,13 +47,114 @@ const getImagePath = (motor) => {
                             </p>
                             <!-- Add more details as needed -->
                         </div>
-                        <div class="card-footer d-flex justify-content-end">
-                            <button class="btn btn-primary rounded-pill" @click="reserveMotorcycle">
+                        <div class="reserve  d-flex justify-content-end">
+                            <button class="btn" @click="reserveMotorcycle">
                                 Reserve Now
                             </button>
                         </div>
                 </div>
+                <div class="col-md-8 col-lg-6 container-fluid">
+                <form class="needs-validation" novalidate="">
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <label for="firstName" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+                            <div class="invalid-feedback">
+                                Valid first name is required.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <label for="contact" class="form-label">Address</label>
+                        <input type="text" class="form-control" id="contact" placeholder="1234 Main St" required="">
+                    </div>
+                    <div class="col-12">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                    </div>
+
+                    <div class="col-12">
+                        <label for="address" class="form-label">Address</label>
+                        <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
+                    </div>
+
+                    <hr class="my-4">
+
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="same-address">
+                        <label class="form-check-label" for="same-address">Shipping address is the same as my billing
+                            address</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="save-info">
+                        <label class="form-check-label" for="save-info">Save this information for next time</label>
+                    </div>
+
+                    <hr class="my-4">
+
+                    <div class="col-md-6">
+                        <label for="password" class="form-label">Current Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="" required="">
+                    </div>
+                    <div class="row gy-3">
+                        <div class="col-md-6">
+                            <label for="password" class="form-label">New Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="" required="">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="password" class="form-label">Confirm Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="" required="">
+                        </div>
+                        <hr class="my-4">
+                    </div>
+
+                    <button class="w-100 btn btn-primary btn-lg" type="submit">Save</button>
+                </form>
             </div>
+                </div>
+                </div>
+
+
     </GuestLayout>
 
 </template>
+<style scoped>
+button{
+    font-weight: bold;
+    color: black;
+  --c: no-repeat linear-gradient(#A555EC 0 0);
+  background:
+    var(--c)  calc(-101% + var(--p,0%)) 100%,
+    var(--c)  calc( 201% - var(--p,0%)) 0;
+  background-size: 50.1% var(--p,.08em);
+  transition: .3s var(--t,0s), background-position .3s calc(.3s - var(--t,0s));
+}
+button:hover{
+  --p: 101%;
+  --t: 0.3s;
+    color: #FFFFD0;
+    font-weight: bold;
+    border-radius: 15px;
+}
+.img {
+    height: auto;
+    transition: 1s ease;
+    box-shadow: 0 4px 8px 0 rgba(208, 156, 250, 0.5), 0 6px 20px 0 rgba(208, 156, 250, 0.5);
+    border-radius: 20px 0 20px 0;
+}
+
+.fixed-height-image {
+    width: 550px;
+    height: 330px; /* Adjust the height as needed */
+    object-fit: cover; /* or object-fit: contain; depending on your preference */
+}
+.img:hover{
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
+    transition: 1s ease;
+    transition: 1s ease;
+}
+</style>
