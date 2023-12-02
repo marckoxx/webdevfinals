1b5e17dec2123017a17521d566ee2aac9f983601
marckoxx <8marcko8@gmail.com>
2023-12-02 17:52:52 +0800

    fix login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -33,243 +33,108 @@ const submit = () => {
     <GuestLayout>
         <div class="container">
 
-<<<<<<< Updated upstream
             <Head title="Log in" />
 
             <div v-if="status" class="mb-4 font-medium text-sm text-success">
                 <!-- Bootstrap does not have a direct equivalent to Tailwind's text-green-600. You may adjust the color using Bootstrap classes or inline styles. -->
                 {{ status }}
-=======
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-        <div class=" row mx-5 g-lg-5 py-5">
-            <div class="img-cont col-lg-7 text-center text-lg-start">
-                <img src="images/logo/logo3.png" alt="" class="img-fluid">
->>>>>>> Stashed changes
             </div>
             <div class="form col-md-6 col-lg-5">
                 <div class="d-flex justify-content-center">
                     <ul class="nav mb-3" id="loginSignupTabs" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" id="login-tab" data-bs-toggle="pill" href="#login" role="tab" aria-controls="login" aria-selected="true">Login</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="signup-tab" data-bs-toggle="pill" href="#signup" role="tab" aria-controls="signup" aria-selected="false">Sign Up</a>
-                    </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link active" id="login-tab" data-bs-toggle="pill" href="#login" role="tab"
+                                aria-controls="login" aria-selected="true">Login</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="signup-tab" data-bs-toggle="pill" href="#signup" role="tab"
+                                aria-controls="signup" aria-selected="false">Sign Up</a>
+                        </li>
                     </ul>
                 </div>
 
-<<<<<<< Updated upstream
-            <form @submit.prevent="submit">
-                <div class="mb-3">
-                    <InputLabel for="email" value="Email" />
+                <form @submit.prevent="submit">
+                    <div class="mb-3">
+                        <InputLabel for="email" value="Email" />
 
-                    <TextInput id="email" type="email" class="form-control mt-1" v-model="form.email" required autofocus
-                        autocomplete="username" />
+                        <TextInput id="email" type="email" class="form-control mt-1" v-model="form.email" required autofocus
+                            autocomplete="username" />
 
-                    <InputError class="mt-2" :message="form.errors.email" />
-                </div>
-
-                <div class="mb-3">
-                    <InputLabel for="password" value="Password" />
-
-                    <TextInput id="password" type="password" class="form-control mt-1" v-model="form.password" required
-                        autocomplete="current-password" />
-
-                    <InputError class="mt-2" :message="form.errors.password" />
-                </div>
-
-                <div class="mb-3 form-check">
-                    <label class="form-check-label">
-                        <Checkbox name="remember" v-model:checked="form.remember" class="form-check-input" />
-                        <span class="ms-2 text-sm text-muted">Remember me</span>
-                    </label>
-                </div>
-
-                <div class="d-flex justify-content-end mt-4">
-                    <Link v-if="canResetPassword" :href="route('password.request')" class="text-muted text-sm">
-                    Forgot your password?
-                    </Link>
-
-                    <PrimaryButton class="ms-4" :class="{ 'disabled': form.processing }" :disabled="form.processing">
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </div>
-=======
-                <div class="tab-content" id="loginSignupTabContent">
-                    <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
-                        <form  class="p-4 p-md-5 border rounded-3" @submit.prevent="submit" >
-                            <!--email-->
-                            <div class="form-floating mb-3">
-                                <TextInput
-                                    id="email"
-                                    type="email"
-                                    class="form-control"
-                                    v-model="form.email"
-                                    required
-                                    autofocus
-                                    autocomplete="username"
-                                />
-                                <InputLabel for="email" value="Email" />
-                                <div class="mt-2">
-                                <InputError class="mt-2" :message="form.errors.email" />
-                                </div>
-                            </div>
-                            <!--password-->
-                            <div class="form-floating mb-3">
-                                <TextInput
-                                    id="password"
-                                    type="password"
-                                    class="form-control"
-                                    v-model="form.password"
-                                    required
-                                    autocomplete="current-password"
-                                />
-                                <InputLabel for="password" value="Password" />
-                                <div class="mt-2">
-                                    <InputError class="mt-2" :message="form.errors.password" />
-                                </div>
-                            </div>
-                        <div class="block mt-4">
-                            <label class="flex items-center">
-                                <Checkbox name="remember" v-model:checked="form.remember" />
-                                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
-                            </label>
-                        </div>
-
-                        <div class="flex items-center justify-end mt-4">
-                            <Button class="btn w-100" style="font-weight: bold;" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                Log in
-                            </Button>
-                            <Link
-                                v-if="canResetPassword"
-                                :href="route('password.request')"
-                                class="underline text-sm text-black dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                            >
-                                Forgot your password?
-                            </Link>
-                            </div>
-                         </form>
+                        <InputError class="mt-2" :message="form.errors.email" />
                     </div>
-                    <div class="tab-pane fade show active" id="signup" role="tabpanel" aria-labelledby="signup-tab">
-                        <form  class="p-4 p-md-5  " @submit.prevent="submit" >
-                            <div class="form-floating mb-3">
-                                <TextInput
-                                    id="name"
-                                    type="text"
-                                    class="form-control"
-                                    v-model="form.name"
-                                    required
-                                    autofocus
-                                    autocomplete="name"
-                                />
-                                <InputLabel for="name" value="Name" />
-                                <div class="mt-2">
-                                    <InputError class="mt-2" :message="form.errors.name" />
-                                </div>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <TextInput
-                                    id="email"
-                                    type="email"
-                                    class="form-control"
-                                    v-model="form.email"
-                                    required
-                                    autocomplete="username"
-                                />
-                                <InputLabel for="email" value="Email" />
-                                <div class="mt-2">
-                                    <InputError class="mt-2" :message="form.errors.email" />
-                                </div>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <TextInput
-                                    id="password"
-                                    type="password"
-                                    class="form-control"
-                                    v-model="form.password"
-                                    required
-                                    autocomplete="new-password"
-                                />
-                                <InputLabel for="password" value="Password" />
-                                <div class="mt-2">
-                                    <InputError class="mt-2" :message="form.errors.password" />
-                                </div>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <TextInput
-                                    id="password_confirmation"
-                                    type="password"
-                                    class="form-control"
-                                    v-model="form.password_confirmation"
-                                    required
-                                    autocomplete="new-password"
-                                />
-                                <InputLabel for="password_confirmation" value="Confirm Password" />
 
-                                <div class="mt-2">
-                                    <InputError class="mt-2" :message="form.errors.password_confirmation" />
-                                </div>
-                            </div>
+                    <div class="mb-3">
+                        <InputLabel for="password" value="Password" />
 
-                            <div class="block mt-4">
-                                <Button class="btn w-100" style="font-weight: bold;" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                    Register
-                                </Button>
-                                <Link
-                                    :href="route('login')"
-                                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                                >
-                                    Already registered?
-                                </Link>
-                            </div>
-                        </form>
+                        <TextInput id="password" type="password" class="form-control mt-1" v-model="form.password" required
+                            autocomplete="current-password" />
+
+                        <InputError class="mt-2" :message="form.errors.password" />
                     </div>
-                </div>
-        </div>
-    </div>
 
->>>>>>> Stashed changes
+                    <div class="mb-3 form-check">
+                        <label class="form-check-label">
+                            <Checkbox name="remember" v-model:checked="form.remember" class="form-check-input" />
+                            <span class="ms-2 text-sm text-muted">Remember me</span>
+                        </label>
+                    </div>
+
+                    <div class="d-flex justify-content-end mt-4">
+                        <Link v-if="canResetPassword" :href="route('password.request')" class="text-muted text-sm">
+                        Forgot your password?
+                        </Link>
+
+                        <PrimaryButton class="ms-4" :class="{ 'disabled': form.processing }" :disabled="form.processing">
+                            Log in
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+        </div>
     </GuestLayout>
 </template>
 <style scoped>
-    .btn{
-        border-radius: 10px 0 10px 0;
-        border: 0;
-        color: #A555EC;
+.btn {
+    border-radius: 10px 0 10px 0;
+    border: 0;
+    color: #A555EC;
     --c: no-repeat linear-gradient(#A555EC 0 0);
     background:
-        var(--c)  calc(-101% + var(--p,0%)) 100%,
-        var(--c)  calc( 201% - var(--p,0%)) 0;
-    background-size: 50.1% var(--p,.08em);
-    transition: .3s var(--t,0s), background-position .3s calc(.3s - var(--t,0s))
-    }
-    .btn:hover {
+        var(--c) calc(-101% + var(--p, 0%)) 100%,
+        var(--c) calc(201% - var(--p, 0%)) 0;
+    background-size: 50.1% var(--p, .08em);
+    transition: .3s var(--t, 0s), background-position .3s calc(.3s - var(--t, 0s))
+}
+
+.btn:hover {
     --p: 101%;
     --t: 0.3s;
     color: #FFFFD0;
     font-weight: 300;
-    }
-    .form {
-        height: 40em;
-        border: 1px solid #A555EC;
-        background-color: rgba(208, 156, 250, 0.5); /* Using rgba to set opacity only for the background */
-        border-radius: 15px;
-        padding: 40px;
-    }
-    #loginSignupTabs .nav-link {
-        color: black; /* Change to the desired text color */
-        border-color: gray; /* Change to the desired border color */
-        transition: background-color 0.3s ease; /* Add a smooth transition effect */
-    }
+}
+
+.form {
+    height: 40em;
+    border: 1px solid #A555EC;
+    background-color: rgba(208, 156, 250, 0.5);
+    /* Using rgba to set opacity only for the background */
+    border-radius: 15px;
+    padding: 40px;
+}
+
+#loginSignupTabs .nav-link {
+    color: black;
+    /* Change to the desired text color */
+    border-color: gray;
+    /* Change to the desired border color */
+    transition: background-color 0.3s ease;
+    /* Add a smooth transition effect */
+}
 
 
-    #loginSignupTabs .nav-link.active {
-        border-bottom: 3px solid;
-        color: #A555EC; /* Change to the desired text color for active tab */
-    }
-</style>
+#loginSignupTabs .nav-link.active {
+    border-bottom: 3px solid;
+    color: #A555EC;
+    /* Change to the desired text color for active tab */
+}</style>
 
